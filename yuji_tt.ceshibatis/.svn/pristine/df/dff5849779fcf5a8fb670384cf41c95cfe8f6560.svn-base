8ba4d31e56d9835a859a6397e3a2ce2ed9e85781
Administrator <Administrator@DESKTOP-HACTMRA>
2019-12-16 11:09:56 +0800

    11111

---
+++ yuji_tt.ceshibatis/.svn/pristine/df/dff5849779fcf5a8fb670384cf41c95cfe8f6560.svn-base
@@ -0,0 +1,13 @@
+package com.yuji.testmybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestmybatisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ yuji_tt.ceshibatis/src/main/java/com/yuji/testmybatis/TestmybatisApplication.java
@@ -0,0 +1,14 @@
+package com.yuji.testmybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestmybatisApplication {
+
+    //111222sqlsssssssmainsql
+    public static void main(String[] args) {
+        SpringApplication.run(TestmybatisApplication.class, args);
+    }
+
+}
